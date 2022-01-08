568a416703e19a154dac8051fbcec99f2498b0ae
Pavel Surkov <surkovv.pavel@gmail.com>
2022-01-08 14:41:12 +0700

    adds correct img load

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import pic from './images/favicon.png';
+
+const App = () => {
+	return (
+		<div>
+			<p>Application</p>
+			<img src={pic} />
+		</div>
+	);
+};
+
+export default App;
