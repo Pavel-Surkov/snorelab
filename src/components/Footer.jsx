d14f6a886f90a1b2f926004b89815a0155d7fbdf
Pavel Surkov <surkovv.pavel@gmail.com>
2022-01-14 12:07:27 +0700

    adds media to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,7 +41,7 @@ const Footer = () => {
 										<img src={logo} alt="logo" />
 									</a>
 									<p>The Snoring Management App for iOS and Android</p>
-									<div className="footer__social-links">
+									<div className="d-none d-lg-flex footer__social-links">
 										<a href="#" className="link footer__social-link" aria-label="twitter link">
 											<div>
 												<svg
@@ -433,6 +433,59 @@ const Footer = () => {
 													fill="white"
 												/>
 											</svg>
+										</a>
+									</div>
+									<div className="d-lg-none footer__social-links">
+										<a href="#" className="link footer__social-link" aria-label="twitter link">
+											<div>
+												<svg
+													width="31"
+													height="30"
+													viewBox="0 0 31 30"
+													fill="none"
+													xmlns="http://www.w3.org/2000/svg"
+												>
+													<path
+														d="M11.7887 28C8.55374 28 5.53817 27.0591 3 25.4356C5.15499 25.5751 8.95807 25.2411 11.3236 22.9848C7.76508 22.8215 6.16026 20.0923 5.95094 18.926C6.25329 19.0426 7.6953 19.1826 8.50934 18.856C4.4159 17.8296 3.78793 14.2373 3.92748 13.141C4.695 13.6775 5.99745 13.8641 6.50913 13.8174C2.69479 11.0882 4.06703 6.98276 4.74151 6.09635C7.47882 9.88867 11.5812 12.0186 16.6564 12.137C16.5607 11.7174 16.5102 11.2804 16.5102 10.8316C16.5102 7.61092 19.1134 5 22.3247 5C24.0025 5 25.5144 5.71275 26.5757 6.85284C27.6969 6.59011 29.3843 5.97507 30.2092 5.4432C29.7934 6.93611 28.4989 8.18149 27.7159 8.64308C27.7224 8.65878 27.7095 8.62731 27.7159 8.64308C28.4037 8.53904 30.2648 8.18137 31 7.68256C30.6364 8.52125 29.264 9.91573 28.1377 10.6964C28.3473 19.9381 21.2765 28 11.7887 28Z"
+														fill="#1DA1F2"
+													/>
+												</svg>
+											</div>
+										</a>
+										<a href="#" className="link footer__social-link" aria-label="facebook link">
+											<div>
+												<svg
+													width="30"
+													height="30"
+													viewBox="0 0 30 30"
+													fill="none"
+													xmlns="http://www.w3.org/2000/svg"
+												>
+													<circle
+														cx="15"
+														cy="15"
+														r="13.125"
+														fill="url(#paint0_linear_944_15001)"
+													/>
+													<path
+														d="M19.8879 19.014L20.4709 15.3095H16.8236V12.9066C16.8236 11.8928 17.3322 10.9041 18.9658 10.9041H20.625V7.7503C20.625 7.7503 19.1199 7.5 17.6815 7.5C14.6764 7.5 12.7141 9.27463 12.7141 12.486V15.3095H9.375V19.014H12.7141V27.9698C13.3844 28.0725 14.0702 28.125 14.7688 28.125C15.4675 28.125 16.1533 28.0725 16.8236 27.9698V19.014H19.8879Z"
+														fill="white"
+													/>
+													<defs>
+														<linearGradient
+															id="paint0_linear_944_15001"
+															x1="15"
+															y1="1.875"
+															x2="15"
+															y2="28.0471"
+															gradientUnits="userSpaceOnUse"
+														>
+															<stop stopColor="#18ACFE" />
+															<stop offset="1" stopColor="#0163E0" />
+														</linearGradient>
+													</defs>
+												</svg>
+											</div>
 										</a>
 									</div>
 								</div>
