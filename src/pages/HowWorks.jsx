d86d7ca3cc7fd35a71ec292abf9f7fd71914e092
Pavel Surkov <surkovv.pavel@gmail.com>
2022-01-16 20:31:12 +0700

    adds start section to howworks page

--- src/pages/HowWorks.jsx
+++ src/pages/HowWorks.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HowWorksMain } from '../components/HowWorksMain';
 import { HowWorksSelect } from '../components/HowWorksSelect';
 import { HowWorksMake } from '../components/HowWorksMake';
+import { HowWorksStart } from '../components/HowWorksStart';
 
 export const HowWorks = () => {
 	return (
@@ -9,6 +10,7 @@ export const HowWorks = () => {
 			<HowWorksMain />
 			<HowWorksSelect />
 			<HowWorksMake />
+			<HowWorksStart />
 		</main>
 	);
 };
