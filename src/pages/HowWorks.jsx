351d0caa20ed826a7f6e42d495ccc91bc7860334
Pavel Surkov <surkovv.pavel@gmail.com>
2022-01-16 14:46:22 +0700

    adds make block

--- src/pages/HowWorks.jsx
+++ src/pages/HowWorks.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { HowWorksMain } from '../components/HowWorksMain';
 import { HowWorksSelect } from '../components/HowWorksSelect';
+import { HowWorksMake } from '../components/HowWorksMake';
 
 export const HowWorks = () => {
 	return (
 		<main className="how-works">
 			<HowWorksMain />
 			<HowWorksSelect />
+			<HowWorksMake />
 		</main>
 	);
 };
