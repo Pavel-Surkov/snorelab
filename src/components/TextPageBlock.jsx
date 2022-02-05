3c220fa656cda5ddbe019cecc9eee156bdbf1e8b
Pavel Surkov <surkovv.pavel@gmail.com>
2022-02-05 23:26:13 +0700

    adds privacy page

---
+++ src/components/TextPageBlock.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TextPageBlock = ({ title, content }) => {
+	return (
+		<div className="text-page__block">
+			<h3 className="title title_size-s text-page__block-title">{title}</h3>
+			<div className="text-page__block-content" dangerouslySetInnerHTML={{ __html: content }} />
+		</div>
+	);
+};
--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PrivacyPage } from '../components/PrivacyPage';
 
 export const Privacy = () => {
 	return (
 		<main className="privacy">
-			<div />
+			<PrivacyPage />
 		</main>
 	);
 };
