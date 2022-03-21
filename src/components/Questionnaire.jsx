c8519c8075980a5fbc354f7c230545acf3fd249f
Pavel Surkov <surkovv.pavel@gmail.com>
2022-03-21 22:25:30 +0700

    adds integration with wp api for questionnaires

--- src/components/Questionnaire.jsx
+++ src/components/Questionnaire.jsx
@@ -1,31 +1,59 @@
 import React from 'react';
 //const { createElement, render } = wp.element;
 
-export const Questionnaire = ({ questions, btnTitle }) => {
-	return (
-		<form className="questionnaire-form">
-			<ol className="questionnaire-list">
-				{questions.map(question => {
-					return (
-						<li className="questionnaire-list__item" key={question.id}>
-							<p>{question.title}</p>
-							{question.answers.map(answer => (
-								<div className="questionnaire-list__item-answer" key={answer.id}>
-									<input type="radio" id={answer.id} name={answer.name} value={answer.value} />
-									<label className="radio" htmlFor={answer.id}>
-										{answer.value}
-									</label>
-								</div>
-							))}
-						</li>
-					);
-				})}
-			</ol>
-			<div className="questionnaire-form__submit-wrapper">
-				<button type="submit" className="btn btn-arrow questionnaire-form__submit">
-					<div>{btnTitle}</div>
-				</button>
-			</div>
-		</form>
-	);
+export const Questionnaire = ({ questionsType, questions, btnTitle }) => {
+  let questionsArray = [];
+
+  console.log(questionsType, questions);
+
+  if (questionsType && questions) {
+    questionsArray = questions[0].acf[`${questionsType}_questions`];
+  }
+
+  if (!questions) {
+    return <h2 className="title title_size-l">Loading...</h2>;
+  }
+
+  return (
+    <form className="questionnaire-form">
+      <ol className="questionnaire-list">
+        {questionsArray.map((question, i) => {
+          return (
+            <li
+              className="questionnaire-list__item"
+              key={question.question_slug}
+            >
+              <p>{question.question_title}</p>
+              {question.question_answers.map((answer, idx) => {
+                return (
+                  <div
+                    className="questionnaire-list__item-answer"
+                    key={answer[`${questionsType}_question_answer`]}
+                  >
+                    <input
+                      type="radio"
+                      id={String(i) + idx}
+                      name={question.question_slug}
+                      value={answer[`${questionsType}_question_answer`]}
+                    />
+                    <label className="radio" htmlFor={String(i) + idx}>
+                      {answer[`${questionsType}_question_answer`]}
+                    </label>
+                  </div>
+                );
+              })}
+            </li>
+          );
+        })}
+      </ol>
+      <div className="questionnaire-form__submit-wrapper">
+        <button
+          type="submit"
+          className="btn btn-arrow questionnaire-form__submit"
+        >
+          <div>{btnTitle}</div>
+        </button>
+      </div>
+    </form>
+  );
 };
--- src/components/StopBangQuestionnaire.jsx
+++ src/components/StopBangQuestionnaire.jsx
@@ -1,49 +1,83 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 //const { createElement, render } = wp.element;
 import { Questionnaire } from './Questionnaire';
-import { stopBangQuestions } from '../helpers/variables';
+// import { stopBangQuestions } from '../helpers/variables';
 
 export const StopBangQuestionnaire = () => {
-	return (
-		<section className="questionnaire">
-			<div className="questionnaire-wrapper">
-				<div className="container">
-					<h1 className="title title_size-m questionnaire-title">STOP-Bang Questionnaire</h1>
-					<div className="questionnaire-text">
-						<p>
-							The STOP-Bang questionnaire is the favoured sleep apnea screening tool in the medical
-							profession.
-						</p>
-						<p>
-							Simply answer the eight yes/no questions below to find out how at risk of obstructive sleep
-							apnea you are.
-						</p>
-						<p>
-							You can{' '}
-							<a className="link" href="#">
-								read more about identifying obstructive sleep apnea
-							</a>{' '}
-							in&nbsp;SnoreLab’s Insights area.
-						</p>
-						<p>
-							You can also assess your sleep apnea risk by looking at your level of&nbsp;daytime fatigue
-							using the{' '}
-							<a className="link" href="#">
-								Epworth Sleepiness Scale
-							</a>.
-						</p>
-					</div>
-					<Questionnaire questions={stopBangQuestions} btnTitle="Access the risk" />
-					<div className="questionnaire-change">
-						<button type="button" className="btn btn-arrow questionnaire-change__prev">
-							<div>Prev</div>
-						</button>
-						<button type="button" className="btn btn-arrow btn_gray-text questionnaire-change__next">
-							<div>Next</div>
-						</button>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  const stopbang = useFetch(
+    'https://snorelab.ux-mind.pro/wp-json/wp/v2/pages?slug=stopbang',
+    {}
+  );
+
+  if (!stopbang.data) {
+    return (
+      <section className="questionnaire">
+        <div className="questionnaire-wrapper">
+          <div className="container">
+            <h1 className="title title_size-m questionnaire-title">
+              STOP-Bang Questionnaire
+            </h1>
+            <h2 className="title title_size-l">Loading...</h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="questionnaire">
+      <div className="questionnaire-wrapper">
+        <div className="container">
+          <h1 className="title title_size-m questionnaire-title">
+            STOP-Bang Questionnaire
+          </h1>
+          <div className="questionnaire-text">
+            <p>
+              The STOP-Bang questionnaire is the favoured sleep apnea screening
+              tool in the medical profession.
+            </p>
+            <p>
+              Simply answer the eight yes/no questions below to find out how at
+              risk of obstructive sleep apnea you are.
+            </p>
+            <p>
+              You can{' '}
+              <a className="link" href="#">
+                read more about identifying obstructive sleep apnea
+              </a>{' '}
+              in&nbsp;SnoreLab’s Insights area.
+            </p>
+            <p>
+              You can also assess your sleep apnea risk by looking at your level
+              of&nbsp;daytime fatigue using the{' '}
+              <a className="link" href="#">
+                Epworth Sleepiness Scale
+              </a>
+              .
+            </p>
+          </div>
+          <Questionnaire
+            questionsType={stopbang.data[0].slug}
+            questions={stopbang.data}
+            btnTitle="Access the risk"
+          />
+          <div className="questionnaire-change">
+            <button
+              type="button"
+              className="btn btn-arrow questionnaire-change__prev"
+            >
+              <div>Prev</div>
+            </button>
+            <button
+              type="button"
+              className="btn btn-arrow btn_gray-text questionnaire-change__next"
+            >
+              <div>Next</div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
