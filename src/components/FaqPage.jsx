26a64cf4d41115c07516ed9993077dccb4efa1cf
Pavel Surkov <surkovv.pavel@gmail.com>
2022-04-24 00:50:11 +0700

    adds faq output from wp

--- src/components/FaqPage.jsx
+++ src/components/FaqPage.jsx
@@ -1,23 +1,53 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 //const { createElement, render } = wp.element;
 import { Accordion } from './Accordion';
-import faqElementsArr from '@/helpers/faqContent';
+// import faqElementsArr from '@/helpers/faqContent';
 
 export const FaqPage = () => {
-	return (
-		<section className="faq-page">
-			<div className="faq-page__wrapper">
-				<div className="container">
-					<h1 className="title title_fade title_size-l faq-page__title">FAQ</h1>
-					<span className="faq-page__subtitle">Frequently Asked Questions</span>
-					<div className="faq-page__block">
-						{/* Renders formed FAQ sections */}
-						{faqElementsArr.map(({ title, elements }) => (
-							<Accordion key={title} title={title} elements={elements} />
-						))}
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  const faqElements = useFetch(
+    'https://snorelab.ux-mind.pro/wp-json/wp/v2/pages?slug=faq',
+    {}
+  );
+
+  console.log(faqElements);
+
+  if (!faqElements.data) {
+    return (
+      <section className="faq-page">
+        <div className="faq-page__wrapper">
+          <div className="container">
+            <h1 className="title title_fade title_size-l faq-page__title">
+              FAQ
+            </h1>
+            <span className="faq-page__subtitle">
+              Frequently Asked Questions
+            </span>
+            <div className="faq-page__block">Loading...</div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="faq-page">
+      <div className="faq-page__wrapper">
+        <div className="container">
+          <h1 className="title title_fade title_size-l faq-page__title">FAQ</h1>
+          <span className="faq-page__subtitle">Frequently Asked Questions</span>
+          <div className="faq-page__block">
+            {/* Renders formed FAQ sections */}
+            {faqElements.data[0].acf.faq_sections.map((element) => (
+              <Accordion
+                key={element.faq_section_title}
+                title={element.faq_section_title}
+                elements={element.faq_section_items}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
